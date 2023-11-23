0d4276c0891a0e6d696219bfe5a2a5dd70379ec0
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-11-23 21:35:00 +0530

    Distinct path done

--- Assignment 2/Distinct Path.cpp
+++ Assignment 2/Distinct Path.cpp
@@ -5,21 +5,49 @@
 #include <algorithm>
 using namespace std;
 
+// int distinctPathRecursion(int m, int n, vector<vector<int>>& grid, int i, int j) {
+//     if (i == m - 1 && j == n - 1) {
+//         return 1;
+//     }
+//     if (i >= m || j >= n) {
+//         return 0;
+//     }
+//     if (grid[i][j] == 1) {
+//         return 0;
+//     }
+//     return distinctPathRecursion(m, n, grid, i + 1, j) + distinctPathRecursion(m, n, grid, i, j + 1);
+// }
+
 void distinctPath(int m, int n, vector<vector<int>>& grid) {
     int** dp = new int*[m];
     for (int i = 0; i < m; i++) {
         dp[i] = new int[n];
-        dp[i][0] = 1;
     }
-    for (int j = 0; j < n; j++) {
-        dp[0][j] = 1;
-    }
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < n; i++) {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    dp[m - 1][n - 1] = 1;
+    for (int i = m - 2; i >= 0; i--) {
+        if (grid[i][n - 1] == 1) {
+            dp[i][n - 1] = 0;
+        } else {
+            dp[i][n - 1] = dp[i + 1][n - 1];
         }
     }
-    cout << dp[m - 1][n - 1] << endl;
+    for (int j = n - 2; j >= 0; j--) {
+        if (grid[m - 1][j] == 1) {
+            dp[m - 1][j] = 0;
+        } else {
+            dp[m - 1][j] = dp[m - 1][j + 1];
+        }
+    }
+    for (int i = m - 2; i >= 0; i--) {
+        for (int j = n - 2; j >= 0; j--) {
+            if (grid[i][j] == 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+    }
+    cout << dp[0][0] << endl;
 }
 
 int main() {
